c87b80c03a90e1a761d612859ce55a56196b9d86
JJeCho <james.je.cho@gmail.com>
2024-01-02 16:13:12 -0800

    Basic Functionality
    Added some other sorting algorithms. Added numerical values to the display of the visualizer

--- src/SortVisualizer.js
+++ src/SortVisualizer.js
@@ -1,12 +1,28 @@
-import React, { useState, useEffect, useRef } from 'react';
-import * as d3 from 'd3';
-
+import React, { useState, useEffect, useRef } from "react";
+import * as d3 from "d3";
+import "./SortVisualizer.css";
+/*
+WIP: Add stop sort functionality
+Fix CSS for responsiveness and better appearance
+*/
 const SortingVisualizer = () => {
   const [data, setData] = useState([]);
   const [arraySize, setArraySize] = useState(20);
+  const [animationSpeed, setAnimationSpeed] = useState(10); // Initial animation speed
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState("bubbleSort"); // Initial algorithm selection
+  const [isSorting, setIsSorting] = useState(false);
   const svgRef = useRef(null);
-  const maxSvgWidth = 50 * 25;
-  const padding = 2;
+  const maxSvgWidth = 1800;
+  const padding = 10;
+
+  // Reset animation after sort with new generated array
+  /*
+  useEffect(() => {
+    if (!isSorting) {
+      setData(generateRandomArray(arraySize));
+    }
+  }, [arraySize, isSorting]);
+  */
 
   function generateRandomArray(length) {
     return Array.from({ length }, () => Math.floor(Math.random() * 100) + 1);
@@ -17,8 +33,13 @@ const SortingVisualizer = () => {
   };
 
   const handleArraySizeChange = (e) => {
-    const newSize = parseInt(e.target.value, 10);
+    let newSize = parseInt(e.target.value, 10);
+    newSize = Math.min(newSize, 150);
     setArraySize(newSize);
+  };
+
+  const handleAnimationSpeedChange = (e) => {
+    setAnimationSpeed(e.target.value);
   };
 
   async function bubbleSort() {
@@ -31,7 +52,9 @@ const SortingVisualizer = () => {
         if (array[i] > array[i + 1]) {
           [array[i], array[i + 1]] = [array[i + 1], array[i]];
           swapped = true;
-          await new Promise((resolve) => setTimeout(resolve, 100));
+          await new Promise((resolve) =>
+            setTimeout(resolve, 1000 / animationSpeed)
+          );
           setData([...array]);
         }
       }
@@ -39,8 +62,161 @@ const SortingVisualizer = () => {
     } while (swapped);
   }
 
-  const startSorting = () => {
-    bubbleSort();
+  async function insertionSort() {
+    let array = [...data];
+    const n = array.length;
+    for (let i = 1; i < n; i++) {
+      let current = array[i];
+      let j = i - 1;
+      while (j >= 0 && array[j] > current) {
+        array[j + 1] = array[j];
+        j--;
+      }
+      array[j + 1] = current;
+      await new Promise((resolve) =>
+        setTimeout(resolve, 1000 / animationSpeed)
+      );
+      setData([...array]);
+    }
+  }
+
+  async function selectionSort() {
+    let array = [...data];
+    const n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < n; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (minIndex !== i) {
+        [array[i], array[minIndex]] = [array[minIndex], array[i]];
+        await new Promise((resolve) =>
+          setTimeout(resolve, 1000 / animationSpeed)
+        );
+        setData([...array]);
+      }
+    }
+  }
+
+  async function quickSort(array, start = 0, end = array.length - 1) {
+    if (start >= end) {
+      return;
+    }
+    let index = await partition(array, start, end);
+    await Promise.all([
+      quickSort(array, start, index - 1),
+      quickSort(array, index + 1, end),
+    ]);
+  }
+
+  async function partition(array, start, end) {
+    let pivotIndex = start;
+    let pivotValue = array[end];
+    for (let i = start; i < end; i++) {
+      if (array[i] < pivotValue) {
+        [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+        await new Promise((resolve) =>
+          setTimeout(resolve, 1000 / animationSpeed)
+        );
+        setData([...array]);
+        pivotIndex++;
+      }
+    }
+
+    [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
+    await new Promise((resolve) => setTimeout(resolve, 1000 / animationSpeed));
+    setData([...array]);
+    return pivotIndex;
+  }
+
+  async function mergeSort(array, start, end) {
+    if (start < end) {
+      const mid = Math.floor((start + end) / 2);
+      await mergeSort(array, start, mid);
+      await mergeSort(array, mid + 1, end);
+      await merge(array, start, mid, end);
+    }
+  }
+
+  async function merge(array, start, mid, end) {
+    let leftArray = array.slice(start, mid + 1);
+    let rightArray = array.slice(mid + 1, end + 1);
+    let i = 0,
+      j = 0,
+      k = start;
+
+    while (i < leftArray.length && j < rightArray.length) {
+      if (leftArray[i] <= rightArray[j]) {
+        array[k++] = leftArray[i++];
+      } else {
+        array[k++] = rightArray[j++];
+      }
+      await new Promise((resolve) =>
+        setTimeout(resolve, 1000 / animationSpeed)
+      );
+      setData([...array]);
+    }
+
+    while (i < leftArray.length) {
+      array[k++] = leftArray[i++];
+      await new Promise((resolve) =>
+        setTimeout(resolve, 1000 / animationSpeed)
+      );
+      setData([...array]);
+    }
+
+    while (j < rightArray.length) {
+      array[k++] = rightArray[j++];
+      await new Promise((resolve) =>
+        setTimeout(resolve, 1000 / animationSpeed)
+      );
+      setData([...array]);
+    }
+  }
+
+  async function startQuickSort() {
+    let array = [...data];
+    await quickSort(array);
+  }
+  async function startMergeSort() {
+    let array = [...data];
+    await mergeSort(array, 0, array.length - 1);
+  }
+
+  const handleAlgorithmChange = (e) => {
+    setSelectedAlgorithm(e.target.value);
+  };
+
+  const handleStopSorting = () => {
+    setIsSorting(false);
+  };
+
+  const startSorting = async () => {
+    setIsSorting(true);
+
+    switch (selectedAlgorithm) {
+      case "bubbleSort":
+        await bubbleSort();
+        break;
+      case "insertionSort":
+        await insertionSort();
+        break;
+      case "selectionSort":
+        await selectionSort();
+        break;
+      case "quickSort":
+        await startQuickSort();
+        break;
+      case "mergeSort":
+        await startMergeSort();
+        break;
+      default:
+        break;
+    }
+
+    setIsSorting(false);
   };
 
   useEffect(() => {
@@ -49,41 +225,92 @@ const SortingVisualizer = () => {
 
   useEffect(() => {
     const svgWidth = arraySize > 50 ? maxSvgWidth : arraySize * 25;
-
-    const svg = d3.select(svgRef.current).attr('height', 150).attr('width', svgWidth);
-
-    svg.selectAll('*').remove();
-
-    const rectWidth = arraySize > 50 ? maxSvgWidth / arraySize : 20;
-
+    const svgHeight = 200;
+  
+    const svg = d3
+      .select(svgRef.current)
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr("preserveAspectRatio", "xMidYMid meet");
+  
+    svg.selectAll("*").remove();
+  
+    const rectWidth =
+      arraySize > 50 ? (maxSvgWidth - (arraySize - 1) * padding) / arraySize : 20;
+  
     svg
-      .selectAll('rect')
+      .selectAll("g")
       .data(data)
       .enter()
-      .append('rect')
-      .attr('x', (d, i) => i * (rectWidth+padding))
-      .attr('y', (d) => 150 - d)
-      .attr('width', rectWidth)
-      .attr('height', (d) => d)
-      .attr('fill', 'steelblue');
+      .append("g")
+      .attr("transform", (d, i) => `translate(${i * (rectWidth + padding)}, 0)`)
+      .append("rect")
+      .attr("y", (d) => svgHeight - d)
+      .attr("width", rectWidth)
+      .attr("height", (d) => d)
+      .attr("fill", "steelblue");
+  
+    svg
+      .selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("x", (d, i) => i * (rectWidth + padding) + rectWidth / 2)
+      .attr("y", (d) => svgHeight - d - 5)
+      .attr("text-anchor", "middle")
+      .text((d) => d)
+      .attr("font-size", "12px")
+      .attr("fill", "black");
   }, [data, arraySize, maxSvgWidth, padding]);
-
-
+  
+  
   return (
-    <div>
-      <div>
-        <label htmlFor="arraySize">Array Size:</label>
-        <input
-          type="number"
-          id="arraySize"
-          value={arraySize}
-          onChange={handleArraySizeChange}
-          min="1"
-        />
-        <button onClick={generateNewArray}>Generate New Array</button>
+    <div id="sorting-visualizer-container">
+      <div id="sorting-visualizer-options">
+        <div id="sorting-visualizer-array">
+          <label htmlFor="arraySize">Array Size:</label>
+          <input
+            type="number"
+            id="arraySize"
+            value={arraySize}
+            onChange={handleArraySizeChange}
+            min="1"
+          />
+          <button onClick={generateNewArray}>Generate New Array</button>
+        </div>
+        <div id="sorting-visualizer-speed">
+          <label htmlFor="speedSlider">Animation Speed:</label>
+          <input
+            type="range"
+            id="speedSlider"
+            value={animationSpeed}
+            min="1"
+            max="100"
+            step="1"
+            onChange={handleAnimationSpeedChange}
+          />
+        </div>
+        <div id="sorting-visualizer-algorithm">
+          <label htmlFor="algorithmSelect">Select Sorting Algorithm:</label>
+          <select
+            id="algorithmSelect"
+            value={selectedAlgorithm}
+            onChange={handleAlgorithmChange}
+          >
+            <option value="bubbleSort">Bubble Sort</option>
+            <option value="insertionSort">Insertion Sort</option>
+            <option value="selectionSort">Selection Sort</option>
+            <option value="quickSort">Quick Sort</option>
+            <option value="mergeSort">Merge Sort</option>
+          </select>
+        </div>
+        <button id="sorting-visualizer-button" onClick={startSorting}>
+          Start Sorting
+        </button>
       </div>
-      <svg ref={svgRef}></svg>
-      <button onClick={startSorting}>Start Sorting</button>
+      <div id="sorting-visualizer-svg">
+        <svg id="sorting-svg" ref={svgRef}></svg>
+      </div>
     </div>
   );
 };
